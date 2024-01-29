448475ec46b73fe4b5fa4af92dceaf3eef167d20
connorClements <75251412+connorClements@users.noreply.github.com>
2024-01-29 21:19:27 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -33,9 +33,9 @@ export default defineConfig({
     },
     build: {
         // Set the base path if your app is not hosted at the root of the domain
-        base: "/",
+        base: "/public/",
         // Set the public path if you are deploying to a non-root path
-        assetsDir: "public",
+        // assetsDir: "public",
         terserOptions: {
             compress: {
                 drop_console: true,
